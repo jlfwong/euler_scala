843c425a9fa8ddad5b93f35f4bb6056128d66435
Jamie Wong <jamie.lf.wong@gmail.com>
2013-12-14 20:10:14 -0700

    p1 a1 a2

--- p1.scala
+++ p1.scala
@@ -1,8 +1,8 @@
 def a1(max: Int): Int =
-  (for (x <- 1 to max-1 if x % 3 == 0 || x % 5 == 0) yield x).sum
+  (for (x <- 1 until max if x % 3 == 0 || x % 5 == 0) yield x).sum
 
 def a2(max: Int): Int =
-  (1 to max-1).filter(x => x % 3 == 0 || x % 5 == 0).sum
+  (1 until max).filter(x => x % 3 == 0 || x % 5 == 0).sum
 
 println(a1(10))
 println(a2(10))
