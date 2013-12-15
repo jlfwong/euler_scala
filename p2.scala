4b467600840e8b538ecc1adb2df78d8ede03875c
Jamie Wong <jamie.lf.wong@gmail.com>
2013-12-14 18:18:04 -0700

    p2 a2

--- p2.scala
+++ p2.scala
@@ -21,4 +21,29 @@ def fib(): Iterator[Int] = {
 def a1(max: Int): Int =
   fib().takeWhile(x => x < max).filter(x => x % 2 == 0).sum
 
+def fibcb(cb: (Int => Boolean)) {
+  var a = 1
+  var b = 1
+  if (!cb(a)) return
+  if (!cb(b)) return
+  while (true) {
+    val temp = a
+    a = b
+    b = b + temp
+    if (!cb(b)) return
+  }
+}
+
+def a2(max: Int): Int = {
+  var sum = 0
+  fibcb(x => {
+    if (x < max) {
+      if (x % 2 == 0) sum += x
+      true
+    } else false
+  })
+  sum
+}
+
 println(a1(4000000))
+println(a2(4000000))
