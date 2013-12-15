b5de2d2e740252424173651294987da2f373fd9e
Jamie Wong <jamie.lf.wong@gmail.com>
2013-12-14 20:10:30 -0700

    p1 a1

--- p4.scala
+++ p4.scala
@@ -1,10 +1,10 @@
 def a1(digits: Int): Long = {
   val low =  Math.round(Math.pow(10, digits - 1))
-  val high = Math.round(Math.pow(10, digits) - 1)
+  val high = Math.round(Math.pow(10, digits))
 
   (for {
-    x <- (low to high)
-    y <- (x to high)
+    x <- (low until high)
+    y <- (x until high)
     prod = x * y
     if prod.toString == prod.toString.reverse
   } yield prod).max
