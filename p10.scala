f4fceb3c3aa5e1b2cf2d144edf09bc2513d59612
Jamie Wong <jamie.lf.wong@gmail.com>
2013-12-16 00:46:10 -1000

    p10 a1

--- p10.scala
+++ p10.scala
@@ -1,16 +1,17 @@
-import scala.collection.mutable.Map
+import scala.collection.mutable.BitSet
 
-def sieve(max: Long): List[Long] = {
-  val isPrime = Map[Long, Boolean]()
-  2L :: (3L to max by 2L filter { i =>
-    if (isPrime.getOrElse(i, true)) {
-      i to max by i foreach { isPrime(_) = false }
+def sieve(max: Int): List[Int] = {
+  val composite = BitSet()
+  2 :: (3 to max by 2 filter { i =>
+    if (composite(i)) false
+    else {
+      i to max by i foreach { composite += _ }
       true
-    } else false
-  } toList)
+    }
+  }).toList
 }
 
-def a1(max: Long): Long = sieve(max).sum
+def a1(max: Int): Long = sieve(max).map(_.toLong).sum
 
 println(a1(10))
 println
