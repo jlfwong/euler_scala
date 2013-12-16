2f391bcf4862d6865f415a03bc15d336466c90e4
Jamie Wong <jamie.lf.wong@gmail.com>
2013-12-16 02:26:18 -1000

    p7 a2

--- p7.scala
+++ p7.scala
@@ -3,13 +3,27 @@ def primes(): Iterator[Int] = {
   var next = 3
   Iterator(2) ++ Iterator.continually {
     while (primes.exists(next % _ == 0)) next += 2
-    primes = next :: primes
+    primes = primes ++ List(next)
     next
   }
 }
 
+def primes2(): Iterator[Int] = {
+  var primes: List[Int] = Nil
+  Iterator(2) ++ Stream.from(3, 2).filter{ n =>
+    if (primes.exists(n % _ == 0)) false
+    else {
+      primes = primes ++ List(n)
+      true
+    }
+  }
+}
+
 def a1(n: Int): Int = primes.take(n).toList.last
+def a2(n: Int): Int = primes2.take(n).toList.last
 
 println(a1(6))
+println(a2(6))
 println
 println(a1(10001))
+println(a2(10001))
